db1896038a7ff1c4cfd0d60392c02dbad44468c7
Norden-Tenzin <nordentenzin29@gmail.com>
2024-02-03 14:55:52 +0530

    Initial Commit

---
+++ guestList/guestListApp.swift
@@ -0,0 +1,17 @@
+//
+//  guestListApp.swift
+//  guestList
+//
+//  Created by Tenzin Norden on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct guestListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
